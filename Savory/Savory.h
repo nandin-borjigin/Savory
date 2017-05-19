b56faf14ab2832311980688918fc0b7527d8b854
Nandiin <nandiin@163.com>
2017-05-20 00:30:06 +0800

    new project

---
+++ Savory/Savory.h
@@ -0,0 +1,19 @@
+//
+//  Savory.h
+//  Savory
+//
+//  Created by Nandiin Borjigin on 20/05/2017.
+//  Copyright © 2017 Nandiin Borjigin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Savory.
+FOUNDATION_EXPORT double SavoryVersionNumber;
+
+//! Project version string for Savory.
+FOUNDATION_EXPORT const unsigned char SavoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Savory/PublicHeader.h>
+
+
