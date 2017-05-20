d734528a9438ab520c045cedd3acec2f109f673c
Nandiin <nandiin@163.com>
2017-05-20 19:22:15 +0800

    Added simple example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Savory
+
+class ViewController: UIViewController, SavoryViewDelegate {
+    // Inherit SavoryViewDelegate Protocol
+    // One may provide some standalone struct/class to inherit it
+    
+    @IBOutlet weak var savoryView: SavoryView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        savoryView.stateProvider = SimpleStateProvider([.expanded, .collapsed, .expanded, .collapsed])
+        // set the stateProvider
+        // anything conforms to protocol SavoryStateProvider would be fine.
+        
+        savoryView.savoryDelegate = self
+        // set the savoryDelegate
+        // not necessarily be view controller itself
+        // anything conforms to protocol SavoryViewDelegate would be fine.
+        
+        savoryView.headerIdentifier = "header"
+        savoryView.bodyIdentifier = "body"
+        // set the reuse identifiers for header cell and body cell
+        // these two identifiers should be somehow registered to the savoryView
+        // for this example, they are registered in the storyboard
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func headerCell(forPanelAt index: SavoryPanelIndex, in savoryView: SavoryView) -> SavoryHeaderCell {
+        // provide a header cell for (index)-th panel
+        let cell = savoryView.dequeueReusableHeaderCell(forPanelAt: index)
+        // SavoryView provides a method for dequeueing reusable cells for certain panel index.
+        
+        // customize the dequeued cell as needed.
+        cell.textLabel?.text = "Header \(index)"
+        cell.selectionStyle = .none
+        return cell
     }
-
+    
+    func bodyCell(forPanelAt index: SavoryPanelIndex, in savoryView: SavoryView) -> SavoryBodyCell {
+        // provide a body cell for (index)-th panel
+        return savoryView.dequeueReusableBodyCell(forPanelAt: index)
+    }
 
 }
 
