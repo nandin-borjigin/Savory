4ff694e682f8b4fd96124c2c981c3366e0a0616a
Nandiin <nandiin@163.com>
2017-05-20 13:19:18 +0800

    Refactored test spec

--- SavoryTests/SavoryViewDelegateSpec.swift
+++ SavoryTests/SavoryViewDelegateSpec.swift
@@ -49,41 +49,28 @@ class SavoryViewDelegateSpec: QuickSpec {
             delegate = DummyDelegate()
         }
         describe("indexPathFor") {
-            let indexPath = { IndexPath(row: $0, section: 0) }
-            context("header") {
-                context("at 0") {
-                    it("returns 0 - 0") {
-                        expect(delegate.indexPathFor(headerAt: 0, in: view)) == indexPath(0)
+            
+            let indexPathFor: (String, Int) -> (returns: (Int, Int) -> (), dummy: ()) = { (type, i) in
+                return ({ (j, k) in
+                    context("\(type) at \(i) in a SavoryView with first panel expanded ant others collapsed") {
+                        it("returns \(j) - \(k)") {
+                            if type == "header" {
+                                expect(delegate.indexPathFor(headerAt: i, in: view)) == IndexPath(row: k, section: j)
+                            } else {
+                                expect(delegate.indexPathFor(bodyAt: i, in: view)) == IndexPath(row: k, section: j)
+                            }
+                        }
                     }
-                }
-                context("after an expanded panel") {
-                    it("returns 0 - 2") {
-                        expect(delegate.indexPathFor(headerAt: 1, in: view)) == indexPath(2)
-                    }
-                }
-                context("after an expanded panel and an collapsed panel") {
-                    it("returns 0 - 3") {
-                        expect(delegate.indexPathFor(headerAt: 2, in: view)) == indexPath(3)
-                    }
-                }
+                }, ())
             }
-            context("detail") {
-                context("at 0") {
-                    it("returns 0 - 1") {
-                        expect(delegate.indexPathFor(bodyAt: 0, in: view)) == indexPath(1)
-                    }
-                }
-                context("after an expanded panel") {
-                    it("returns 0 - 3") {
-                        expect(delegate.indexPathFor(bodyAt: 1, in: view)) == indexPath(3)
-                    }
-                }
-                context("after an expanded panel and an collapsed panel") {
-                    it("returns 0 - 4") {
-                        expect(delegate.indexPathFor(bodyAt: 2, in: view)) == indexPath(4)
-                    }
-                }
-            }
+            
+            indexPathFor("header", 0).returns(0, 0)
+            indexPathFor("header", 1).returns(0, 2)
+            indexPathFor("header", 2).returns(0, 3)
+            
+            indexPathFor("body", 0).returns(0, 1)
+            indexPathFor("body", 1).returns(0, 3)
+            indexPathFor("body", 2).returns(0, 4)
         }
         describe("cellType") {
             
